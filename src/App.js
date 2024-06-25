b67e09bf3739bc37878632ee457c5bcad91e9e29
Dorian Kita <dorian.kita90@gmail.com>
2024-06-25 20:36:15 +0200

    created component and route it with react-router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+// createBrowserRouter is mapping function to create map between components
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/Home";
+
+createBrowserRouter([{ path: "/", element: <HomePage /> }, {}]);
+
 function App() {
   return <div></div>;
 }
