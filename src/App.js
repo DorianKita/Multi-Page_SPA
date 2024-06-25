cf57bf4d232d0dcc8bc5e6c60ad7ccf2d24ea0c3
Dorian Kita <dorian.kita90@gmail.com>
2024-06-25 20:57:18 +0200

    created navigation via Link component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-// createBrowserRouter is mapping function to create map between components
-import { createBrowserRouter } from "react-router-dom";
+// createBrowserRouter is mapping function to create map between components,
+// RouterProvider is component that is wrapping all routers
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
 
-createBrowserRouter([{ path: "/", element: <HomePage /> }, {}]);
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
 
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
