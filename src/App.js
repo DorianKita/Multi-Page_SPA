436cec040b61a3512e066ad908cc6c8584cf3169
Dorian Kita <dorian.kita90@gmail.com>
2024-06-25 22:06:12 +0200

    created navigation with Root component to show on every page, and styled it with module css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 // createBrowserRouter is mapping function to create map between components,
 // RouterProvider is component that is wrapping all routers
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/products", element: <ProductsPage /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
 ]);
 
 function App() {
