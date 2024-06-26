125b9d3c54710f576295ceedfc50e4406badb349
Dorian Kita <dorian.kita90@gmail.com>
2024-06-26 21:19:41 +0200

    added dynamics paths to dynamic created Link components for navigating throughth different products

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,12 +1,26 @@
+import { Link } from "react-router-dom";
+
+import classes from "../components/MainNavigation.module.css";
+
+const PRODUCTS = [
+  { id: "p1", title: "Product 1" },
+  { id: "p2", title: "Product 2" },
+  { id: "p3", title: "Product 3" },
+];
+
 function ProductsPage() {
   return (
     <>
       <h1>The Products Page</h1>
-      <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
-      </ul>
+      <div className={classes.header}>
+        <ul className={classes.list}>
+          {PRODUCTS.map((item) => (
+            <li key={item.id}>
+              <Link to={`/products/${item.id}`}>{item.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
