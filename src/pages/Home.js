db0b149049fbf3558109af052b22c8fbbba81355
Dorian Kita <dorian.kita90@gmail.com>
2024-06-26 20:42:21 +0200

    used programmatically navigating for practice

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    setTimeout(() => navigate("/products"), 5000);
+  }
+
   return (
     <>
       <h1>My home page</h1>
       <p>
         Check our <Link to="/products">Products</Link>.
       </p>
+      <button onClick={navigateHandler}>Navigate to products.</button>
     </>
   );
 }
